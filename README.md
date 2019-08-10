1d9e53ea4c4fbc4e04cc2a14a6f797d732bb08c0
noobpk <leethanhphuc@gmail.com>
2019-08-10 21:42:27 +0700

    Turtle graphics

---
+++ README.md
@@ -0,0 +1,3 @@
+# Some funny codes are collected
+
+- [x] illusion - a script using Turtle graphics.
---
+++ illusion.py
@@ -0,0 +1,12 @@
+#!/bin/bash/python
+from turtle import *
+color('red')
+bgcolor('purple')
+begin_fill()
+for i in range(600):
+		forward(i)
+		left(100)
+		speed(1000)
+		fillcolor("blue")
+end_fill()
+done()	
